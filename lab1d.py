f4f258f7e41d70899228cac24248e0184046a0e4
Mehtaash Kaur <gs5389094@gmail.com>
2026-09-16 20:01:31 +0000

    Completed Lab1D

--- lab1d.py
+++ lab1d.py
@@ -5,24 +5,23 @@
 # Usage: python3 lab1d.py
 
 #TO-DO 1:
-#	Create a variable called "name" and assign it the value of your name.
-# Use the string method .upper() to convert the name to upper case.
-# Create another variable called “age”, the value of “age” should be your age
-# The script, when executed, should print out "How are you yourname? Happy xxth birthday!" To print this output use .format() method.
-name="Mehtaash"
+name="mehtaash"
 str.upper(name)
 age=18
 print("How are you {}? Happy {}th birthday!".format(name,age)) 
 
 #TO-DO 2:
-# Create a variable called "words".
-# The value of words should be "The quick brown fox jumps over the lazy dog".
-# Use indexing to return the first and 17th charecters of "words" to the user.
 words="The quick brown fox jumps over the lazy dog"
+print("The first character is",words[0])
+print("The 17th character is",words[16])
 
 #TO-DO 3:
-# Use negative indexing to return the words "jumps" and "quick" from "words" to the user.
+slice1=words[-23:-18]
+print(slice1)
+slice2=words[-39:-34]
+print(slice2)
 
 #TO-DO 4:
-# Use slicing to retun everything between index 2-15 to the user.
-# Print "uick brown foxs ju" from "words".
+slice3=words[2:15]
+print(slice3)
+print(words[5:22])
