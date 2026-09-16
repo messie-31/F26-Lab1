b20d954614b7b8f37b130f5f2bf5ab565085b675
Mehtaash Kaur <gs5389094@gmail.com>
2026-09-16 20:08:11 +0000

    Completed Lab1E

--- lab1e.py
+++ lab1e.py
@@ -1,9 +1,6 @@
-
-# Add comments before you do anything else.
-
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: Mehtaash Kaur
+# Date: 2026/09/16
 # Purpose: Use string methods and f-string formating.
 # Usage: python3 lab1e.py
 
@@ -14,4 +11,8 @@
 # The value of "stock" should also be a decimal number of your own choice.
 # Print the product of `quantity` and `stock` with 4 spaces before the answer using the module % formatting.
 # Then print the product of `quantity` and `stock` with 7 spaces before the answer and make sure the answer only goes to hundreadths (-.--) using the module % formatting.
-
+quantity=6.8
+stock=57.510
+product=quantity*stock
+print("The product of quantity and stock is: %4.0f" %product)
+print("The product of quantity and stock is: %7.2f" %product)
